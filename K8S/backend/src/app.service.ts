c301d1a2a714ee8aa3947928e07546862bfa541f
Alejandro Toloza <alejandro.toloza@isistan.unicen.edu.ar>
2025-11-26 18:14:57 -0300

    con readme

--- K8S/backend/src/app.service.ts
+++ K8S/backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService { // <-- ¡La palabra "export" es crucial!
   getHello(): string {
-    return 'Hello World!';
+    return 'TOLOZA KUBERNETS Hello !!!';
   }
 }
